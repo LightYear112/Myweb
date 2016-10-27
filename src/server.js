a648c4062dbb6dcb0f621b191e7c874745a661ab
LightYear112 <lycsliuyang@gmail.com>
2016-10-27 17:36:21 +0800

    webpack

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ var port = 8081;
 app.get('/',function(req,res){
   res.sendfile('./src/index.html');
 })
+app.get('/login',function(req,res){
+  res.send('login success');
+})
 var server = app.listen(port,function(){
     var host = server.address().address
     console.log("running on %s:%s",host,port)
